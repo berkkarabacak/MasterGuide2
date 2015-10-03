4ff61cde867120d64d162f9e4c6be6eb4890d685
Berk Karabacak <berkkarabacak@gmail.com>
2015-10-03 20:15:26 +0300

    ghjgj

--- app/src/main/java/com/hackathon/masterguide/Main3Activity.java
+++ app/src/main/java/com/hackathon/masterguide/Main3Activity.java
@@ -1,33 +1,26 @@
 package com.hackathon.masterguide;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.widget.Button;
+import android.view.Window;
 
-public class Main2Activity extends AppCompatActivity {
+public class Main3Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
+       // Main3Activity.requestWindowFeature(Window.FEATURE_NO_TITLE);
 
-        setContentView(R.layout.activity_main2);
-        final Button button = (Button) findViewById(R.id.traveller);
-        button.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
-                startActivity(new Intent(Main2Activity.this, Main3Activity.class));
-            }
-        });
+        setContentView(R.layout.activity_main3);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main2, menu);
+        getMenuInflater().inflate(R.menu.menu_main3, menu);
         return true;
     }
 
