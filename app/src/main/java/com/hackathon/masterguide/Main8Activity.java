e15cb177af7d62682359790da6cd55d64e34b589
Umut Uz <umutuz7@gmail.com>
2015-10-04 11:18:56 +0300

    My list eklendi.

--- app/src/main/java/com/hackathon/masterguide/Main8Activity.java
+++ app/src/main/java/com/hackathon/masterguide/Main8Activity.java
@@ -1,11 +1,13 @@
 package com.hackathon.masterguide;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.widget.Button;
 
 public class Main8Activity extends AppCompatActivity {
 
@@ -13,15 +15,11 @@ public class Main8Activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main8);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+        final Button button6 = (Button) findViewById(R.id.button6);
+        button6.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                //Toast.makeText(getApplicationContext(), "umut", Toast.LENGTH_SHORT).show();
+                startActivity(new Intent(Main8Activity.this, MainActivity.class));
             }
         });
     }
